699067ba1c5bae12431cb666e1e4fe027a733b99
SupunMA <supunmadhusanka62.com>
2021-11-24 15:08:43 +0530

    Added client table

--- resources/views/layouts/adminLayout.blade.php
+++ resources/views/layouts/adminLayout.blade.php
@@ -22,6 +22,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
    
     <!-- Theme style -->
     <link rel="stylesheet" href={{ URL::asset('dist/css/adminlte.min.css'); }}>
+
+    <!-- DataTables -->
+  <link rel="stylesheet" href={{ URL::asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css'); }}>
+  <link rel="stylesheet" href={{ URL::asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css'); }}>
+  <link rel="stylesheet" href={{ URL::asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css'); }}>
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -515,6 +520,47 @@ scratch. This page gets rid of all links and provides the needed markup only.
     })
     </script>
     
+<!-- DataTables  & Plugins -->
+
+<script src={{ URL::asset('plugins/datatables/jquery.dataTables.min.js'); }}></script>
+<script src={{ URL::asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js'); }}></script>
+<script src={{ URL::asset('plugins/datatables-responsive/js/dataTables.responsive.min.js'); }}></script>
+<script src={{ URL::asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js'); }}></script>
+<script src={{ URL::asset('plugins/datatables-buttons/js/dataTables.buttons.min.js'); }}></script>
+<script src={{ URL::asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js'); }}></script>
+<script src={{ URL::asset('plugins/jszip/jszip.min.js'); }}></script>
+<script src={{ URL::asset('plugins/pdfmake/pdfmake.min.js'); }}></script>
+<script src={{ URL::asset('plugins/pdfmake/vfs_fonts.js'); }}></script>
+<script src={{ URL::asset('plugins/datatables-buttons/js/buttons.html5.min.js'); }}></script>
+<script src={{ URL::asset('plugins/datatables-buttons/js/buttons.print.min.js'); }}></script>
+<script src={{ URL::asset('plugins/datatables-buttons/js/buttons.colVis.min.js'); }}></script>
+
+
+<!-- AdminLTE for demo purposes -->
+<script src="{{ URL::asset('dist/js/demo.js'); }}"></script>
+<!-- Page specific script -->
+<script>
+    $(function () {
+        $("#example1").DataTable({
+            "responsive": true,
+            "lengthChange": false,
+            "autoWidth": false,
+            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+        $('#example2').DataTable({
+            "paging": true,
+            "lengthChange": false,
+            "searching": false,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true,
+        });
+    });
+
+</script>
+
+
 </body>
 
 </html>
