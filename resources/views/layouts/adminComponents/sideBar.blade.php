1595ad6009a49f67cdfdb7e9e619b28c6351c5b9
SupunMA <supunmadhusanka62.com>
2021-12-08 20:09:10 +0530

    fix old url

--- resources/views/layouts/adminComponents/sideBar.blade.php
+++ resources/views/layouts/adminComponents/sideBar.blade.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{route('admin.home')}}" class="brand-link">
         <img src="https://drive.google.com/uc?export=view&id=17oMlgpisY5HADXWqOh9jfu5WzDANcE_f"
             alt="MMC Logo" class="brand-image img-circle elevation-3" style="opacity: 1">
         <span class="brand-text font-weight-light">Admin Panel</span>
