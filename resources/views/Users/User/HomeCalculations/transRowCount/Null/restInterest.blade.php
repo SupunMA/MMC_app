8f1888cc60589bd9c299c48ac61f1e67877d00a2
SupunMA <supunmadhusanka62.com>
2021-12-15 21:38:11 +0530

    delete unwanted lines

--- resources/views/Users/User/HomeCalculations/transRowCount/Null/restInterest.blade.php
+++ resources/views/Users/User/HomeCalculations/transRowCount/Null/restInterest.blade.php
@@ -8,30 +8,14 @@
             $loanGotDate = new DateTime($loanGotDateCal);
             $currentDate = new DateTime($newDate);
             $interval = $loanGotDate->diff($currentDate);
-            //$days = $interval->format('%a');
-            
-            //dd($interval->m,$interval->d);
-            
+           
             $moreDays = $interval->d;
             $moreMonths = $interval->m;
             $moreYears = $interval->y;
 
-                //current Loan Paying month and year
-            // $paidDateToGetTheMonth = Carbon\Carbon::createFromFormat('Y-m-d', $newDate);
-            // $monthName = $paidDateToGetTheMonth->format('m');
-            // $year = $paidDateToGetTheMonth->format('Y');
-
-                //get Due date from loan table
-            // $date = Carbon\Carbon::createFromFormat('Y-m-d', $item->loanDate);
-            // $dueDate = $date->format('d');
-
-                //get today date
-            // $date = Carbon\Carbon::createFromFormat('Y-m-d', $newDate);
-            // $newDateDay = $date->format('d');
-
             if ($moreMonths > 0 && $moreDays > 0 && $moreYears > 0) {
                 $calAllInterest = (($item->loanAmount * ($item->loanRate/100)) * (($moreMonths + 1) + ($moreYears * 12)));
-                //$calAllInterest =0;
+                
             }
 
             if ($moreMonths == 0 && $moreDays > 0 && $moreYears > 0) {
@@ -65,13 +49,4 @@
 
 ?>
 
-
 {{$calAllInterest}}
-
-{{-- {{$moreDays}}
-<br>
-{{$moreMonths}}
-<br>
-{{$moreYears}} --}}
-
-{{-- {{$diff_in_months}} --}}
