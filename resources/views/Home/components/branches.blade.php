16bda5d9a583b0e6e67d71ee73f46445eb361431
SupunMA <supunmadhusanka62.com>
2022-01-23 13:44:07 +0530

    Homepage branches retreiving

--- resources/views/Home/components/branches.blade.php
+++ resources/views/Home/components/branches.blade.php
@@ -5,24 +5,26 @@
     <br>
     <div class="row justify-content-center">
 
-
-        <div class="card mr-3 mb-3 wow fadeInUp" style="width: 18rem;">
-            <img class="card-img-top" src="homePage/img/logot.png" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">
-                    <h4>ii</h4>
-                </h5>
-                <p class="card-text">
-                    <ul>
-                        <li><strong>Address:-</strong> </li>
-                        <li><strong>Telephone:-</strong> </li>
-                    </ul>
-                </p>
-                <a href="#" target="_blank" class="btn btn-primary">View on map</a>
+        @foreach ($bdata as $oneBranch )
+            
+            <div class="card mr-3 mb-3 wow fadeInUp" style="width: 18rem;">
+                <img class="card-img-top" src="homePage/img/logot.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        <h4>{{$oneBranch->branchName}}</h4>
+                    </h5>
+                    <p class="card-text">
+                        <ul>
+                            <li><strong>Address:- {{$oneBranch->branchAddress}}</strong> </li>
+                            <li><strong>Telephone:- {{$oneBranch->branchTP}}</strong> </li>
+                        </ul>
+                    </p>
+                    <a href="{{$oneBranch->branchLocation}}" target="_blank" class="btn btn-primary"><i class="ion-ios-location"></i> &nbsp; View on map</a>
+                </div>
             </div>
-        </div>
 
-
+        @endforeach
+        
     </div>
     <br>
 </section>
