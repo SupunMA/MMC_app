532240d99bf205fb3df0f6f27dacd334b987fcd6
SupunMA <supunmadhusanka62.com>
2022-01-24 14:10:57 +0530

    reset/forgot pwd msg

--- resources/views/other/forgotAndRegister.blade.php
+++ resources/views/other/forgotAndRegister.blade.php
@@ -3,12 +3,14 @@
 @section('content')
 <div class="container text-center">
     <div class="row justify-content-center">
+
         <div class="col-xl-6 col-lg-6 col-md-6 col-sm-9 col-11">
             <h4>call to </h4> <h3><b>Madhushanka Micro Credit <br> (Pvt) Ltd</b></h3> <h4>company.</h4> 
             <p> <i>(You do not have permission to reset your password and create new account.!)</i> 
             <br>If you want to Create New account or Rest your password, <br>please contact the company.
             <br>Thank you!</p>
         </div>
+        
     </div>
 </div>
 @endsection
