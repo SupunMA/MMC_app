0a3386e4c12421deaea44277323ba00ae05e1d3b
SupunMA <supunmadhusanka62.com>
2022-07-06 18:20:28 +0530

    change footer year

--- resources/views/layouts/adminComponents/footer.blade.php
+++ resources/views/layouts/adminComponents/footer.blade.php
@@ -5,6 +5,6 @@
         No Plug, Just Play! 
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2021 <a href="https://thakshane.lk" target="_blank">Thakshane.lk</a>.</strong> All rights
+    <strong>Copyright &copy; 2022 <a href="https://thakshane.lk" target="_blank">Thakshane.lk</a>.</strong> All rights
     reserved.
 </footer>
