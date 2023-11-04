1f1799dd5a9cf51dc50e7383b7c59fa818cd2f3a
SupunMA <supunmadhusanka62@gmail.com>
2023-11-04 22:42:49 +0530

    commented unwanted css and remove sweetalert2 css

--- resources/views/layouts/adminComponents/lib/Style.blade.php
+++ resources/views/layouts/adminComponents/lib/Style.blade.php
@@ -18,22 +18,20 @@ href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&d
     <!-- iCheck for checkboxes and radio inputs -->
     <link rel="stylesheet" href={{ URL::asset('adminPages/plugins/icheck-bootstrap/icheck-bootstrap.min.css'); }}>
     <!-- Bootstrap Color Picker -->
-    <link rel="stylesheet" href={{ URL::asset('adminPages/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css'); }}>
+    {{-- <link rel="stylesheet" href={{ URL::asset('adminPages/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css'); }}> --}}
     <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href={{ URL::asset('adminPages/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'); }}>
+    {{-- <link rel="stylesheet" href={{ URL::asset('adminPages/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'); }}> --}}
     <!-- Select2 -->
     <link rel="stylesheet" href={{ URL::asset('adminPages/plugins/select2/css/select2.min.css'); }}>
     <link rel="stylesheet" href={{ URL::asset('adminPages/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css'); }}>
     <!-- Bootstrap4 Duallistbox -->
-    <link rel="stylesheet" href={{ URL::asset('adminPages/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css'); }}>
+    {{-- <link rel="stylesheet" href={{ URL::asset('adminPages/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css'); }}> --}}
     <!-- BS Stepper -->
-    <link rel="stylesheet" href={{ URL::asset('adminPages/plugins/bs-stepper/css/bs-stepper.min.css'); }}>
+    {{-- <link rel="stylesheet" href={{ URL::asset('adminPages/plugins/bs-stepper/css/bs-stepper.min.css'); }}> --}}
     <!-- dropzonejs -->
-    <link rel="stylesheet" href={{ URL::asset('adminPages/plugins/dropzone/min/dropzone.min.css'); }}>
+    {{-- <link rel="stylesheet" href={{ URL::asset('adminPages/plugins/dropzone/min/dropzone.min.css'); }}> --}}
 
 <!--Toastr-->
-    <!-- SweetAlert2 -->
-    <link rel="stylesheet" href={{ URL::asset('adminPages/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css'); }}>
     <!-- Toastr -->
     <link rel="stylesheet" href={{ URL::asset('adminPages/plugins/toastr/toastr.min.css'); }}>
 
