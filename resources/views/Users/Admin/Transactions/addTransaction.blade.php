823df269a9a6492d5be280d83b476cdb39e97abc
SupunMA <supunmadhusanka62@gmail.com>
2023-11-09 22:03:17 +0530

    included deduction card to add new transaction page

--- resources/views/Users/Admin/Transactions/addTransaction.blade.php
+++ resources/views/Users/Admin/Transactions/addTransaction.blade.php
@@ -8,26 +8,65 @@
         <i class="fas fa-list-ul mr-1"></i>
         <b>View All Transactions</b>
     </a>
+<br>
 
-    
-        
-        {{-- <div class="row"> --}}
 
+
+            {{-- <h5 class="form-title">Are you Going to?</h5> --}}
+            <div class="row" style="margin: 20px 0px">
+            <div class="custom-control custom-radio" style="margin-right: 20px">
+              <input class="custom-control-input custom-control-input-success" value="card1" onclick="showCard('card1')" id="show1" type="radio"  name="showcard" checked>
+              <label for="show1" class="custom-control-label">Pay the loan</label>
+            </div>
+
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input custom-control-input-danger" value="card2" onclick="showCard('card2')" id="show2" type="radio" name="showcard">
+              <label for="show2" class="custom-control-label">Cut off Debt </label>
+            </div>
+ </div>
             {{-- Client Details form --}}
-            @include('Users.Admin.Transactions.components.newTransaction')
-           
-            {{-- Client Password form --}}
-            
 
-        {{-- </div>  --}}
+
+            <div id="card1" style="display: block;">
+
+                @include('Users.Admin.Transactions.components.newTransaction')
+
+            </div>
+
+            <div id="card2" style="display: none;">
+
+                @include('Users.Admin.Transactions.components.newDeductionTransaction')
+
+            </div>
+            {{-- Client Password form --}}
+
+
+
             {{-- End of Row --}}
 
-    
+
             {{-- End of Form --}}
 
 
 </div>
 @endsection
+
+@push('specificJs')
+<script>
+    function showCard(cardId) {
+        var card1 = document.getElementById('card1');
+        var card2 = document.getElementById('card2');
+
+        if (cardId === 'card1') {
+            card1.style.display = 'block';
+            card2.style.display = 'none';
+        } else if (cardId === 'card2') {
+            card1.style.display = 'none';
+            card2.style.display = 'block';
+        }
+    }
+</script>
+@endpush
 
 @section('header')
 Add New Transaction
