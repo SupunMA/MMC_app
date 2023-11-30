88c7192006b621e587e40a52b072ef9c54b0cf5b
SupunMA <supunmadhusanka62@gmail.com>
2023-11-30 22:29:47 +0530

    remove column filter

--- resources/views/layouts/adminComponents/lib/specific-js/dataTables.blade.php
+++ resources/views/layouts/adminComponents/lib/specific-js/dataTables.blade.php
@@ -21,7 +21,7 @@
             "lengthChange": false,
             "autoWidth": false,
             "order": [[0, 'desc']],
-            "buttons": ["excel", "pdf", "print","copy", "colvis"] //"csv","copy"
+            "buttons": ["excel", "pdf", "print","copy"] //"csv","copy", "colvis
         }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
         // $('#example2').DataTable({
         //     "paging": true,
